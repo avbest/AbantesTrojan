6458ff39161c3ee9b3e4160e3a903e5b70c919fc
ElektroKill <skullmaster0103@gmail.com>
2018-04-06 18:27:00 +0200

    Modified Assembly Info

--- Abantes/Properties/AssemblyInfo.cs
+++ Abantes/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Abantes")]
-[assembly: AssemblyDescription("Abantes")]
+[assembly: AssemblyTitle("Adobe Download Manager")]
+[assembly: AssemblyDescription("Adobe Download Manager")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Abantes")]
-[assembly: AssemblyProduct("Abantes")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Adobe Systems Incorporated")]
+[assembly: AssemblyProduct("Adobe Download Manager")]
+[assembly: AssemblyCopyright("Copyright  2015 Adobe Systems Incorporated. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.163")]
+[assembly: AssemblyFileVersion("2.0.0.163")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
